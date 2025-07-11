30599fa359b7708659936e7f3583fe3590443d69
Ismael dos Santos <ismaeldossantoscosta06@gmail.com>
2025-07-10 23:10:08 -0300

    feat: adiciona estrutura inicia com atributos da conta bancaria

--- ContaBancaria/src/ContaTerminal.java
+++ ContaBancaria/src/ContaTerminal.java
@@ -1,8 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+        Scanner leitura =  new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da Agencia! ");
+         agencia = leitura.nextLine();
+
+        System.out.println("Informe o numero da sua conta: ");
+         numero = leitura.nextInt();
+         leitura.nextLine();
+
+        System.out.println("Por favor, informe o seu nome: ");
+         nomeCliente = leitura.nextLine();
+
+
+        System.out.println("Por favor, informe o seu saldo: ");
+         saldo = leitura.nextDouble();
+
+        System.out.println("Olá," + nomeCliente + ",o numero da sua conta é " + numero +
+                ",obrigado por criar uma conta em nosso banco, sua agencia é" + agencia +
+                ", o seu saldo é" + saldo + ",já está disponivel para saque");
+
+
+
 
     }
 }
